2d545cb6c9e1cad0c170ade4b1915faa0167aa0e
Tatsuya <steave6@gmail.com>
2018-12-16 03:49:12 +0900

    #ADD set logs directory.

--- loadTopo/log/index.js
+++ loadTopo/log/index.js
@@ -1,4 +1,11 @@
 var winston = require('winston')
+const fs = require('fs')
+var path = require('path')
+var logDir = path.resolve(__dirname, '../../logs') // directory path you want to set
+if ( !fs.existsSync( logDir ) ) {
+    // Create the directory if it does not exist
+    fs.mkdirSync( logDir );
+}
 
 const logger = winston.createLogger({
   level: 'info',
@@ -8,8 +15,8 @@ const logger = winston.createLogger({
     // - Write to all logs with level `info` and below to `combined.log` 
     // - Write all logs error (and below) to `error.log`.
     //
-    new winston.transports.File({ filename: 'loadTopo-error.log', level: 'error' }),
-    new winston.transports.File({ filename: 'loadTopo-infos.log' })
+    new winston.transports.File({ filename: path.resolve(logDir, 'loadTopo-error.log'), level: 'error' }),
+    new winston.transports.File({ filename: path.resolve(logDir, 'loadTopo-infos.log') })
   ]
 // eslint-disable-next-line semi
 });
