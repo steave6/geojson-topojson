4f7723f3798325feb663dd69aff6a2191e4e6603
Tatsuya <steave6@gmail.com>
2018-12-15 09:32:52 +0900

    #UPDATE require and format  fixed at loadTopo.

--- loadTopo/index.js
+++ loadTopo/index.js
@@ -1,26 +1,26 @@
 #!/usr/bin/env node
-const AWS = require("aws-sdk")
+const AWS = require('aws-sdk')
 const fs = require('fs')
 const path = require('path')
-const log = require(path.resolve(__dirname, './log'));
+const log = require('./log')
 
-const config = require(path.resolve(__dirname, './config.json'))
+const config = require('./config.json')
 AWS.config.update(config)
 
 var docClient = new AWS.DynamoDB.DocumentClient();
 
-log.info("Importing movies into DynamoDB. Please wait.");
+log.info('Importing movies into DynamoDB. Please wait.');
 
 var putItem = function (params) {
     docClient.put(params, function(err, data) {
        if (err) {
-         log.error("Unable to add properties", params.town, ". Error JSON:", JSON.stringify(err, null, 2))
+         log.error('Unable to add properties', params.town, '. Error JSON:', JSON.stringify(err, null, 2))
          if (err.retryable) {
            log.info('Retry on error')
            putItem(params);
          }
        } else {
-           log.info("PutItem succeeded:", params.town);
+           log.info('PutItem succeeded:', params.town);
        }
     });
   }
